83486bc2605858baeeb6a7ca5ccc12cc276af300
Filip Kozlik <filmatkozlik@gmail.com>
2024-10-27 11:51:58 +0100

    Added handling of page indicator component

--- portfolio-website/portfolio-app/src/App.tsx
+++ portfolio-website/portfolio-app/src/App.tsx
@@ -1,18 +1,31 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./App.scss";
 import Home from "./pages/home/Home";
-import Cursor from "./components/cursor/cursor";
 import Skills from "./pages/skills/Skills";
 import Experience from "./pages/experience/Experience";
+import Cursor from "./components/cursor/cursor";
 import PageIndicator from "./components/pageIndicator/pageIndicator";
 
 const App: React.FC = (): JSX.Element => {
   return (
     <div className="app">
-      <Home />
-      <Skills />
-      <Experience />
-      <Cursor />
-      <PageIndicator />
+      <Router>
+        <Cursor />
+        <PageIndicator />
+
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
--- portfolio-website/portfolio-app/src/components/pageIndicator/pageIndicator.tsx
+++ portfolio-website/portfolio-app/src/components/pageIndicator/pageIndicator.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import "./pageIndicator.scss";
 import { PAGES } from "../../utils/consts/pages";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../state/store";
+import { Link } from "react-router-dom";
+import { changePage } from "../../state/page/pageSlice";
 
 const PageIndicator: React.FC = (): JSX.Element => {
   const selectedPage = useSelector(
     (state: RootState) => state.page.selectedPage
   );
+  const dispatch = useDispatch();
+
+  const handlePathChange = (page: string) => {
+    dispatch(changePage(page));
+  };
 
   return (
     <div className="indicator-container">
       {PAGES.map((page, index) => (
-        <div
-          key={index}
-          className={`page-dot ${page === selectedPage ? "active" : ""}`}
-        ></div>
+        <Link to={`/${page}`} onClick={() => handlePathChange(page)}>
+          <div
+            key={index}
+            className={`page-dot ${page === selectedPage ? "active" : ""}`}
+          ></div>
+        </Link>
       ))}
     </div>
   );
